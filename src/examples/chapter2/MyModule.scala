066e003d5c50461102c6fa8bd18418e503f2cad2
Jesse <folde01@users.noreply.github.com>
2020-02-28 22:06:22 +0000

    ch2 - factorial with inner function aka local definition

--- src/examples/chapter2/MyModule.scala
+++ src/examples/chapter2/MyModule.scala
@@ -8,8 +8,24 @@ object MyModule {
     msg.format(n, abs(n))
   }
 
+  def factorial(n: Int): Int = {
+    def go(m: Int, product: Int): Int = {
+      if (m == 0 || m == 1) product
+      else go(m - 1, product * m)
+    }
+    go(n, 1)
+  }
+
+
+
+
   def main(args: Array[String]): Unit = {
     println(absFormat(-5))
     println(absFormat(1))
+
+    println(factorial(0))
+    println(factorial(1))
+    println(factorial(2))
+    println(factorial(3))
   }
 }
